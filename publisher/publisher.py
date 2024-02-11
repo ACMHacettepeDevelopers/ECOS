24c2c85e39075594c34a56318cbfb906ec2555fb
sezaiemrekonuk <sezaikonuk@hacettepe.edu.tr>
2024-02-11 10:45:57 +0300

    publisher with cloud func

--- publisher/publisher.py
+++ publisher/publisher.py
@@ -1,30 +1,55 @@
-import os
-from typing import Tuple, Dict
-
-from dotenv import load_dotenv
 from google.cloud import pubsub_v1
+import functions_framework
 
-load_dotenv()
-
-
-# TODO(developer)
-# project_id = "your-project-id"
-# topic_id = "your-topic-id"
-
-publisher_node = pubsub_v1.PublisherClient()
-
-# The `topic_path` method creates a fully qualified identifier
-# in the form `projects/{project_id}/topics/{topic_id}`
-topic_path = publisher_node.topic_path("gscambulans", "test01")
 
 # Event: id(id), location, patient number, description.
-def publish_alarm(loc, patient_count, desc) -> Tuple[bool, Dict[str, str]]:
+@functions_framework.http
+def publish_alarm(request):
+    publisher_node = pubsub_v1.PublisherClient()
+    topic_path = publisher_node.topic_path("gscambulans", "test01")
+
+    # Set CORS headers for the preflight request
+    if request.method == "OPTIONS":
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Max-Age": "3600",
+        }
+
+        return "", 204, headers
+
+    # Set CORS headers for the main request
+    headers = {"Access-Control-Allow-Origin": "*"}
+
+    request_json = request.get_json(silent=True)
+
+    if request_json is None:
+        return "Invalid JSON", 400, headers
+
+    if request_json and "location" in request_json:
+        loc = request_json["location"]
+    else:
+        return "Missing location", 400, headers
+
+    if request_json and "patient_count" in request_json:
+        patient_count = request_json["patient_count"]
+    else:
+        return "Missing patient count", 400, headers
+
+    if request_json and "description" in request_json:
+        desc = request_json["description"]
+    else:
+        return "Missing description", 400, headers
 
     data_string = f"Location: {loc}, Patient count: {patient_count}, Description: {desc}"
     data = data_string.encode("utf-8")
+
     try:
         snap = publisher_node.publish(topic_path, data)
         print(f"Published messages to {topic_path}.")
-        return True, {"publisher_result": snap.result(), "message": "Published successfully."}
+        return 'OK', 200, headers
     except Exception as e:
-        return False, {"error": str(e)}
+        return f'Error: {e}', 500, headers
